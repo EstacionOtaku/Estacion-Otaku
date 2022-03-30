1e3833b62773ac2917936821df2852a964463cb3
JeanfrancoLA <genkoz98@gmail.com>
2022-03-30 15:40:16 -0500

    Create and linking User Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Hero from "./components/Hero";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <main className="main">
       <Routes>
         <Route path="/" element={<Hero/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        
       </Routes>
     </main>
   );
