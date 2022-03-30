55dacdc1a3120a78e1d353d720b3c04273840bfc
JeanfrancoLA <genkoz98@gmail.com>
2022-03-30 16:50:29 -0500

    Handling user registration errors

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,30 +1,49 @@
 import { useState } from "react";
 import { useAuth } from "../context/authContext";
-
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
     const [user, setUser] = useState({
         email: "",
         password: "",
       });
-
+    const [error, setError] = useState(); 
+    const navigate = useNavigate();
     const {signup} = useAuth();
 
     const handleChange = e => {
         const {target: {name, value}} = e;
         setUser({...user,[name]:value});
     }
-
     const handleSubmit = async e => {
         e.preventDefault();
+        setError('');
         // console.log(user);
-        signup(user.email, user.password);
+        try {
+          await signup(user.email, user.password);
+          navigate("/login");
+        } catch (error) {
+          setError(error.mesagge);
+          if (error.code ==="auth/internal-error"){
+            setError('El correo ingresado es invalido');
+          }
+          else if(error.code === "auth/invalid-email"){
+            setError('El correo ingresado es invalido')
+          }
+          else if (error.code === "auth/weak-password"){
+            setError('La contraseña debe contener más de 6 caracteres');
+          }
+          else if (error.code === "auth/email-already-in-use"){
+            setError('Este correo ya está registrado');
+          }
+        }
     }
 
 
   return (
     <>
       <div>
+        {error && <p>{error}</p>}
         <form onSubmit={handleSubmit}>
           <label htmlFor="email">Email</label>
           <input
