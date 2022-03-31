07f79367f0e2e3085f223b2f84b93a01b6d1a250
bruno <brunoacy12@gmail.com>
2022-03-31 15:12:03 -0500

    ModalConfig con estilos

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,11 +1,9 @@
 import HeaderLanding from "../components/Header/HeaderLanding";
 import Hero from "../components/Hero";
-import ModalConfig from "../components/modals/ModalConfig";
 
 const StarPage = () => {
   return (
     <>
-      <ModalConfig></ModalConfig>
       <HeaderLanding />
       <Hero />
       <div>AQUI VA EL FUT3R GAAAAAAAA :v</div>
