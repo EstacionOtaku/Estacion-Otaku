ce2cada76622f8b3895b5267e4eb690f28f257d8
paulayong <pyongzea@gmail.com>
2022-04-03 19:39:31 -0500

    Footer agregado

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./styles/css/reset.css";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
