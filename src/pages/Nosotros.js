1c9d03496f46fa6cc57f8fcfcc44a514fea88bb3
bruno <brunoacy12@gmail.com>
2022-04-05 01:11:25 -0500

    Agregamos componente Form y Nosotroscars

--- src/pages/Nosotros.js
+++ src/pages/Nosotros.js
@@ -2,12 +2,10 @@ import "../styles/scss/Nosotros.scss";
 import HeaderInicio from "../components/Header/HeaderInicio";
 import Portada from "../assets/Nosotros/portada.png";
 import Logo from "../assets/Nosotros/nk.png";
-import Home from "../assets/Nosotros/home.png";
-import Love from "../assets/Nosotros/love.png";
-import Play from "../assets/Nosotros/play.png";
-import Movie from "../assets/Nosotros/movie.png";
+
 import Footer from "../components/Footer/Footer";
-import Email from "../assets/Nosotros/email.png";
+import FormNosotros from "../components/Form/FormNosotros";
+import NosotrosCards from "../components/Cards/NosotrosCards";
 
 const Nosotros = () => {
   return (
@@ -27,123 +25,8 @@ const Nosotros = () => {
           </p>
         </div>
       </article>
-      <section class="container d-flex align-items-center justify-content-center alto-50 mt-5 ">
-        <div class="row">
-          <div class="col-12 col-lg-9 ">
-            <div class="row ">
-              <div class="col-12 col-lg-8 p-2">
-                <article class="card h-100 bg-light text-white border border-white  border border-3 ">
-                  <div class="card-body">
-                    <div class="d-flex mb-3  ">
-                      <img src={Play} alt="play" />
-                      <div class="ps-3">
-                        <h4 class="m-0 p-2 h4  text-secondary">
-                          Plataforma peruana
-                        </h4>
-                        <p class="m-0 p-2 fs-5 text-secondary">
-                          Somos la primera plataforma low-cost de streaming
-                          enfocado en series y peliculas anime creada en el Perú
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </div>
-              <div class="col-12 col-lg-4 p-2">
-                <article class="card h-100 bg-dark text-white border border-white  border border-3">
-                  <div class="card-body ">
-                    <div class="d-flex mb-3  ">
-                      <div class="ps-3">
-                        <h4 class="m-0 p-2 h4">Variedad</h4>
-                        <p class="m-0 p-2 fs-5">
-                          Ponemos a tu disposición más de 200 series y peliculas
-                          de anime
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </div>
-              <div class="col-12 col-lg-4 p-2">
-                <article class="card h-100 bg-dark text-white border border-white  border border-3">
-                  <div class="card-body ">
-                    <div class="d-flex mb-3  ">
-                      <div class="ps-3">
-                        <h4 class="m-0 p-2 h4">Suscriptores</h4>
-                        <p class="m-0 p-2 fs-5">
-                          Somos más de 2000 personas dentro de nuestra
-                          plataforma
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </div>
-              <div class="col-12 col-lg-4 p-2">
-                <article class="card h-100 bg-dark text-white border border-white  border border-3 ">
-                  <div class="d-flex mb-3  ">
-                    <img src={Love} alt="love" />
-                    <div class="ps-3 ">
-                      <h4 class="m-0 p-2 h4 ">Todo en un solo lugar</h4>
-                      <p class="m-0 p-2 fs-5">
-                        Encuentra una variedad de series y películas de todo
-                        tipo con una experiencia diseñada especialmente para ti
-                      </p>
-                    </div>
-                  </div>
-                </article>
-              </div>
-
-              <div class="col-12 col-lg-8 p-2">
-                <article class="card h-100 bg-light text-white border border-white  border border-3">
-                  <div class="card-body ">
-                    <div class="d-flex mb-3 ">
-                      <img src={Home} alt="home" />
-                      <div class="ps-3">
-                        <h4 class="m-0 p-2 h4 text-secondary ">
-                          Siempre contigo
-                        </h4>
-                        <p class="m-0 p-2 fs-5 text-secondary ">
-                          Te acompañamos donde quiera que vayas para que no te
-                          pierdas ninguna de nuestras novedades
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <form action="form-control">
-        <article class="container d-flex align-items-center  alto-50 text-white  mt-5 mb-5 ">
-          <img class="ms-4" src={Email} alt="mail" />
-          <div class="ps-3 me-5 ">
-            <h2 class="m-0 p-0 h2 ms-4 "> Recibe nuestras novedades </h2>
-          </div>
-
-          <div class="col-auto d-flex  ms-3">
-            <label for="form label" class="visually-hidden">
-              Correo:
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              id="email"
-              placeholder="Email"
-            />
-          </div>
-          <div class="col-auto d-flex  ms-5">
-            <button
-              type="submit"
-              class="btn btn-outline-light btn-border-radius-mb"
-            >
-              Suscribirse
-            </button>
-          </div>
-        </article>
-      </form>
+      <NosotrosCards></NosotrosCards>
+      <FormNosotros></FormNosotros>
       <Footer></Footer>
     </>
   );
