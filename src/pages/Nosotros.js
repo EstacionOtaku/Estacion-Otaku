cfb44eacba4ac769dad8edc51faeb44cce32be5a
unknown <clarissa.palomino@pucp.edu.pe>
2022-04-04 20:41:29 -0500

    Agregando seccion Suscribirse

--- src/pages/Nosotros.js
+++ src/pages/Nosotros.js
@@ -1,13 +1,14 @@
 import HeaderInicio from "../components/Header/HeaderInicio";
-import Logo from "../assets/Nosotros/nk.png"
-import "../styles/scss/Nosotros.scss"
-// import Portada from "---"
+import Logo from "../assets/Nosotros/nk.png";
+import "../styles/scss/Nosotros.scss";
+import Portada from "../assets/Nosotros/portada.png";
+import Email from "../assets/Nosotros/email.png";
 
 const Nosotros = () => {
-  
+   
   return ( <>
   <HeaderInicio></HeaderInicio>
-
+  <img class="portada" src={Portada} alt="p"/>
      <article class="container d-flex align-items-center justify-content-center alto-50 text-white mb-5">
                 
                 <img src={Logo} alt="nc"/>
@@ -19,9 +20,29 @@ const Nosotros = () => {
        </p>
   
 </div>
+</article>
+
+
+<form action="form-control" >
+    <article class="container d-flex align-items-center  alto-50 text-white  mt-5 mb-5 ">
+<img class="ms-4" src={Email} alt="mail"/>
+                <div class="ps-3 me-5 ">
+                <h2 class="m-0 p-0 h2 ms-4 "> Recibe nuestras novedades </h2>
+               </div>
+
+  <div class="col-auto d-flex  ms-3">
+    <label for="form label" class="visually-hidden">Correo:</label>
+    <input type="email" class="form-control" id="email" placeholder="Email"/>
+  </div>
+  <div class="col-auto d-flex  ms-5">
+    <button type="submit" class="btn btn-outline-light btn-border-radius-mb">Suscribirse</button>
+  </div>
 
 </article>
-     </>  );
-}
+</form>
  
+</>  
+     );
+}
+  
 export default Nosotros;
