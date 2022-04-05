c88e0b96a90f0da9ea4cfb17a524a9f6a958a8d0
bruno <brunoacy12@gmail.com>
2022-04-05 17:08:46 -0500

    Instalacion de dependecia jquery para el js de bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/css/reset.css";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
