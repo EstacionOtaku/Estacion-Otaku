c94e6be123c3e78c033cb48540af17358dfe3373
Valeria <Valeria>
2022-04-09 02:05:19 -0500

    ajuste texto

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -9,10 +9,11 @@ const Discover = () => {
         <h3 className="discover__subtitle">Descubre Estación Otaku</h3>
         <h1 className="discover__title">¿Estas listo para disfrutar?</h1>
         <h2 className="discover__text">
-          Descubre Estación Otaky, la nueva aplicación donde podrás disfrutar de
-          tus animes favoritos y conocer nuevos para tu colección. Somos mucho
-          más que una web, somos una comunidad para fans de los animes y la
-          cultura otaku
+          Descubre Estación Otaku, la nueva aplicación web donde podrás
+          disfrutar de tus animes favoritos y conocer nuevos para tu colección.
+          Somos mucho más que una página, somos una comunidad de streaming para
+          fans de los animes. Tenemos animes en una diversidad de categorías.
+          ¿Quieres saber más?
         </h2>
         <div className="discover__button">
           <CtaButton text="Únete gratis" />
