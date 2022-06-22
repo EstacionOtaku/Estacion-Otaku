37a6a5953e49db15434f8a6781528d902753374e
Diego <diegojairhl@gmail.com>
2022-06-22 15:23:14 -0500

    hero section parallax effect on desktop

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,54 @@
-import mobileDecorationImageRight from "../assets/images/hero-anime-right.png";
-import mobileDecorationImageLeft from "../assets/images/hero-anime-left.png";
-import mobileHeroImage from "../assets/Footer/hero-portada.jpeg";
+import animeGroup from "../assets/Hero/anime_group.png";
 import CtaButton from "./Buttons/CtaButton.jsx";
 import "../styles/css/Hero.css";
+import animeOne from "../assets/Hero/anime_1.png";
+import animeTwo from "../assets/Hero/anime_2.png";
+import { useEffect } from "react";
+import { useAnimation, useViewportScroll, motion, useTransform } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const fasterDuration = {
+  visible: { opacity: 1, transition: { duration: 0.4 } },
+  hidden: { opacity: 0 },
+};
+const normalDuration = {
+  visible: { opacity: 1, transition: { duration: 1 } },
+  hidden: { opacity: 0 },
+};
 
 const Hero = () => {
+  const { scrollY } = useViewportScroll();
+  const y1 = useTransform(scrollY, [0, 800], [0, 200]);
+  const y2 = useTransform(scrollY, [0, 600], [0, -200]);
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
   return (
     <>
       <section className="hero">
         <div className="hero-wrapper">
-          <h1 className="hero__cta-text">
+          <motion.h1 className="hero__cta-text" ref={ref} variants={fasterDuration} initial="hidden" animate={control}>
             Tus animes favoritos, <br />
             en un solo lugar
-          </h1>
-          <CtaButton text="Únete gratis" />
+          </motion.h1>
+          <motion.div ref={ref} variants={normalDuration} initial="hidden" animate={control}>
+            <CtaButton text="Únete gratis" />
+          </motion.div>
         </div>
+        <figure className="hero__image-container">
+          <img src={animeGroup} alt="" className="hero__image" />
+          <motion.img src={animeOne} style={{ y: y1, x: y1, top: -156, left: -235 }} alt="" className="animeOne float selector " ref={ref} variants={normalDuration} initial="hidden" animate={control} />
+          <motion.img src={animeTwo} style={{ y: y1, x: y2, top: -56, right: -235 }} alt="" className="animeTwo float" ref={ref} variants={normalDuration} initial="hidden" animate={control} />
+        </figure>
+
         {/* <div className="hero__gradient-background"></div> */}
         {/* <figure className="mobile-hero__image-container">
           <img className="hero__image" src={mobileHeroImage} />
@@ -26,6 +60,7 @@ const Hero = () => {
         <figure className="hero__decoration-right-image-container">
           <img src={mobileDecorationImageRight} alt="" className="hero__decoration-right-image" />
         </figure> */}
+
         <div className="hero__transparency-layer-hero"></div>
       </section>
     </>
@@ -33,3 +68,42 @@ const Hero = () => {
 };
 
 export default Hero;
+
+/**
+ * 
+ * import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
+const boxVariant = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  hidden: { opacity: 0, scale: 0 }
+};
+
+const Box = ({ num }) => {
+
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
+  return (
+    <motion.div
+      className="box"
+      ref={ref}
+      variants={boxVariant}
+      initial="hidden"
+      animate={control}
+    >
+      <h1>Box {num} </h1>
+    </motion.div>
+  );
+};
+
+ */
