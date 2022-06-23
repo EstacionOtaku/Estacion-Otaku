55f378115a2e204248e4cb66375fa6093943d19e
Diego <diegojairhl@gmail.com>
2022-06-22 21:04:17 -0500

    cursor style edited in accordion header

--- src/components/Faq/Accordion.jsx
+++ src/components/Faq/Accordion.jsx
@@ -6,7 +6,7 @@ const Accordion = ({ expanded, setExpanded, i, data }) => {
   const isOpen = i === expanded;
   return (
     <div className="accordion__item">
-      <motion.header initial={false} animate={{ borderBottom: isOpen ? "0px solid transparent" : "1px solid rgba(255, 255, 255, 0.416)" }} onClick={() => setExpanded(isOpen ? false : i)} className="accordion-item__header" key={id}>
+      <motion.header initial={false} animate={{ borderBottom: isOpen ? "0px solid transparent" : "1px solid rgba(255, 255, 255, 0.416)" }} onClick={() => setExpanded(isOpen ? false : i)} style={{ cursor: "pointer" }} className="accordion-item__header" key={id}>
         <h4 className="accordion-item__title">{title}</h4>
         <motion.div animate={{ rotate: isOpen ? 180 : 0 }}>
           <RiArrowDownSLine className="accordion-item__arrow" />
